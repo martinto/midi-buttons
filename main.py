f4510351bdf8efa2184606b08c11d4d6c09c15a4
Martin Tomes <martin@tomes.org.uk>
2023-03-25 17:22:13 +0000

    Now operates generals on the eight combos and couplers on the other three.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import midi
 # button GPIO pins.
 UARTNUMBER=1
 # This can safely be changed.
-MIDICHANNEL=1
+MIDICHANNEL=4
 BUTTONS=[None]*29
 BUTTON_STATE=[1]*29
 
@@ -26,12 +26,18 @@ while True:
     if BUTTONS[pinno].value() != BUTTON_STATE[pinno]:
       BUTTON_STATE[pinno] = BUTTONS[pinno].value()
       if BUTTON_STATE[pinno] == 0:
-        print("Button ", pinno, " down")
-        INSTRUMENT.program_change(pinno)
-  for pinno in range(6, 29): # pin 6-28
+        print("Global ", pinno, " down")
+        INSTRUMENT.control_change(pinno + 1, 1)
+  for pinno in range(6, 9): # pin 6-8
     if BUTTONS[pinno].value() != BUTTON_STATE[pinno]:
       BUTTON_STATE[pinno] = BUTTONS[pinno].value()
       if BUTTON_STATE[pinno] == 0:
-        print("Button ", pinno, " down")
-        INSTRUMENT.program_change(pinno)
+        print("Global ", pinno, " down")
+        INSTRUMENT.control_change(pinno + 1, 1)
+  for pinno in range(9, 12): # pin 9-11
+    if BUTTONS[pinno].value() != BUTTON_STATE[pinno]:
+      BUTTON_STATE[pinno] = BUTTONS[pinno].value()
+      if BUTTON_STATE[pinno] == 0:
+        print("Coupler ", pinno, " down")
+        INSTRUMENT.program_change(pinno + 1)
   sleep(0.001)
