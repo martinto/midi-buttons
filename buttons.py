04c061e6ba4feabb2904c70e5b923d5dd5476084
Martin Tomes <martin@tomes.org.uk>
2025-02-24 12:18:12 +0000

    Send a programme change for each button.

--- buttons.py
+++ buttons.py
@@ -8,6 +8,13 @@ class Buttons:
     _led:Pin
     _is_global = True
 
+    GLOBAL_TOGGLE = 9
+    COMBINATION = 1
+    PED_GT_COUPLER = 10
+    PED_SWELL_COUPLER = 11
+    CANCEL = 12
+    INVALID = 0
+
     def __init__(self, handler) -> None:
         for b in self._buttonpins:
             p = Pin(b, Pin.IN, Pin.PULL_UP)
@@ -17,3 +24,32 @@ class Buttons:
         if self._is_global:
             _led.high()
 
+
+    def _button_type(self, n:int):
+        if n == 0:
+            return Buttons.GLOBAL_TOGGLE
+        elif n < 9:
+            return Buttons.COMBINATION
+        elif n == 9:
+            return Buttons.PED_GT_COUPLER
+        elif n == 10:
+            return Buttons.PED_SWELL_COUPLER
+        elif n == 11:
+            return Buttons.CANCEL
+        else:
+            return Buttons.INVALID
+
+    def midi_pc_number(self, n:int) -> int:
+        btn_t = self._button_type(n)
+        pc = Buttons.INVALID
+        if btn_t == Buttons.GLOBAL_TOGGLE:
+            pc = Buttons.GLOBAL_TOGGLE
+        elif btn_t == Buttons.COMBINATION:
+            pc = n
+        elif btn_t == Buttons.PED_GT_COUPLER:
+            pc = Buttons.PED_GT_COUPLER
+        elif btn_t == Buttons.PED_SWELL_COUPLER:
+            pc = Buttons.PED_SWELL_COUPLER
+        elif btn_t == Buttons.CANCEL:
+            pc = Buttons.CANCEL
+        return pc
--- maintest.py
+++ maintest.py
@@ -12,14 +12,15 @@ UART_1 = 1
 def handle_button_irq(x:Pin, n:int):
     DebouncedSwitch(x, button_pressed, [x, n])
 
-def button_pressed(x):
-    if x[0].value():
-        print(f"{x[1]}: True")
-    else:
-        print(f"{x[1]}: False")
-
 mport = midi.Midi(UART_1, tx=MIDI_TX, rx=MIDI_RX)
 buttons = Buttons(handle_button_irq)
+
+def button_pressed(x):
+    if x[0].value():
+        button_number = x[1]
+        pc = buttons.midi_pc_number(button_number)
+        print(f"{button_number}: {pc}")
+        mport.send_program_change(MIDICHANNEL, pc)
 
 while True:
     sleep(1)
